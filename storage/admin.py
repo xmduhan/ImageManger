d2a363481ea521e22e5422cf41798436a4d3ee36
xmduhan <duhan>
2022-04-20 10:20:56 +0800

    ...

--- storage/admin.py
+++ storage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from storage.models import ArchiveModel
+from storage.models import ArchiveModel, FileModel
 
 # Register your models here.
 
@@ -7,3 +7,10 @@ class StorageAdmin(admin.ModelAdmin):
     """ """
     
 admin.site.register(ArchiveModel, StorageAdmin)
+
+
+class FileAdmin(admin.ModelAdmin):
+    """ """
+    list_display = ['archive', 'filename']
+    
+admin.site.register(FileModel, FileAdmin)
--- storage/models.py
+++ storage/models.py
@@ -1,10 +1,39 @@
+import os
 from distutils import archive_util
 from django.db import models
-import os
+from zipfile import ZipFile
+from django.conf import settings
+
 
 class ArchiveModel(models.Model):
     """ """
     archive = models.FileField(upload_to ='storage/')
 
     def __str__(self):
-       os.path.basename(self.archive.name) 
+       return os.path.basename(self.archive.name) 
+
+    class Meta:
+        verbose_name = "压缩包"
+        verbose_name_plural = "[01].压缩包"
+
+    def save(self, *args, **kwargs):
+        super(ArchiveModel, self).save(*args, **kwargs)
+        filename = os.path.join(settings.MEDIA_ROOT, self.archive.name)
+        with ZipFile(filename, 'r') as zf:
+            for fn in zf.namelist():
+                file_model = FileModel()
+                file_model.filename = fn
+                file_model.archive = self
+                file_model.save()
+
+class FileModel(models.Model):
+    """ """
+    filename = models.CharField('文件名', max_length=1000)
+    archive = models.ForeignKey(ArchiveModel, on_delete=models.CASCADE, verbose_name='压缩包')
+
+    def __str__(self):
+       return self.filename
+
+    class Meta:
+        verbose_name = "文件"
+        verbose_name_plural = "[02].文件"
